dc43cc40bb6c9bdf674b2ca62d5166e801c0af52
aloarte <aloarter@gmail.com>
2016-07-19 15:34:31 +0200

    Incluidos esqueletos en pantalla de ranking y logros con TabLayouts

---
+++ app/src/main/java/com/uc3m/p4r4d0x/emergapp/fragments/RankFragment1.java
@@ -0,0 +1,21 @@
+package com.uc3m.p4r4d0x.emergapp.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.uc3m.p4r4d0x.emergapp.R;
+
+/**
+ * Created by p4r4d0x on 19/07/16.
+ */
+public class RankFragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ranking1,container,false);
+    }
+}
